0b415e975989f5fd7bcb6d73d3f87418b9c28bba
Jumail <jumail@utm.my>
2020-07-14 21:30:05 +0800

    Use FutureProvider (replace FutureBuilder)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'dependencies.dart';
+import 'models/todo.dart';
 import 'screens/todo_list_screen.dart';
 import 'dependencies.dart' as di;
+import 'services/todo_data_service.dart';
 
 void main() {
   di.init();
@@ -11,7 +15,9 @@ void main() {
       title: 'Provider Types',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      home: TodoListScreen(),
+      home: FutureProvider<List<Todo>>(
+          create: (context) => service<TodoDataService>().getTodoList(),
+          child: TodoListScreen()),
     ),
   );
 }
--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,39 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../services/todo_data_service.dart';
-import '../dependencies.dart';
 import '../models/todo.dart';
 
 class TodoListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Todo> _todos; // this is not a state, it is just a convinient variable
+    final _todos = Provider.of<List<Todo>>(context);
 
-    return FutureBuilder<List<Todo>>(
-      future: service<TodoDataService>().getTodoList(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          _todos = snapshot.data;
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('My Todo List'),
-            ),
-            body: ListView.separated(
-              itemCount: _todos.length,
-              separatorBuilder: (context, index) => Divider(
-                color: Colors.blueGrey,
-              ),
-              itemBuilder: (context, index) {
-                final _todo = _todos[index];
-                return ListTile(title: Text(_todo.title));
-              },
-            ),
-          );
-        }
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      },
+    if (_todos == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Todo List (FutureProvider)'),
+      ),
+      body: ListView.separated(
+        itemCount: _todos.length,
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.blueGrey,
+        ),
+        itemBuilder: (context, index) {
+          final _todo = _todos[index];
+          return ListTile(title: Text(_todo.title));
+        },
+      ),
     );
   }
 }
