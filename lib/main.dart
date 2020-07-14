809687ac731c79cd63188555ca0dc2e107ebf563
Jumail <jumail@utm.my>
2020-07-14 21:38:10 +0800

    Use StreamProvider with stream source got from Future.asStream()

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'dependencies.dart';
+import 'models/todo.dart';
 import 'screens/todo_list_screen.dart';
 import 'dependencies.dart' as di;
+import 'services/todo_data_service.dart';
 
 void main() {
   di.init();
 
   runApp(
     MaterialApp(
-      title: 'Provider',
+      title: 'Provider Types',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      home: TodoListScreen(),
+      home: StreamProvider<List<Todo>>(
+        create: (context) =>
+            service<TodoDataService>().getTodoList().asStream(),
+        child: TodoListScreen(),
+      ),
     ),
   );
 }
--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,62 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '../services/todo_data_service.dart';
-import '../dependencies.dart';
 import '../models/todo.dart';
 
-class TodoListScreen extends StatefulWidget {
-  @override
-  _TodoListScreenState createState() => _TodoListScreenState();
-}
-
-class _TodoListScreenState extends State<TodoListScreen> {
-  List<Todo> _todos; // the state
-
-// Define convinient getters and shorthands
-  get todoService => service<TodoDataService>();
-  get todoList => todoService.getTodoList();
-
-  void toggleTodo(int index) async {
-    //Update the status at the database
-    Todo updatedTodo = await todoService.updateTodoStatus(
-        id: _todos[index].id,
-        status: !_todos[index]
-            .completed); // Get the current status from UI and toggle it
-    setState(() => _todos[index].completed = updatedTodo
-        .completed); // Update UI using the updated todo from database
-  }
-
-  void addTodo() async {
-    final newTodo = await todoService.createTodo(
-      todo: Todo(title: 'New Task'),
-    ); // Update server. Id for the new Todo will be given by the server
-
-    setState(() => _todos.add(newTodo)); // Update UI
-  }
-
-  void deleteTodo(int index) async {
-    await todoService.deleteTodo(
-        id: _todos[index].id); // Delete todo at the database
-    setState(() => _todos.removeAt(index)); // Update UI
-  }
-
+class TodoListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Todo>>(
-        future: todoList,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            _todos = snapshot.data;
-            return _buildMainScreen();
-          }
-          return _buildFetchingDataScreen();
-        });
-  }
+    final _todos = Provider.of<List<Todo>>(context);
 
-  Scaffold _buildMainScreen() {
+    if (_todos == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('My Todo List'),
+        title: Text('My Todo List (StreamProvider)'),
       ),
       body: ListView.separated(
         itemCount: _todos.length,
@@ -65,29 +23,8 @@ class _TodoListScreenState extends State<TodoListScreen> {
         ),
         itemBuilder: (context, index) {
           final _todo = _todos[index];
-          return ListTile(
-            title: Text(_todo.title,
-                style: TextStyle(
-                    decoration: _todo.completed
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none)),
-            subtitle: Text('id: ${_todo.id}'),
-            onTap: () async => toggleTodo(index),
-            onLongPress: () async => deleteTodo(index),
-          );
+          return ListTile(title: Text(_todo.title));
         },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async => addTodo(),
-      ),
-    );
-  }
-
-  Scaffold _buildFetchingDataScreen() {
-    return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
       ),
     );
   }
